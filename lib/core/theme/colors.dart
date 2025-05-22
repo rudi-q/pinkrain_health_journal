e5b8b0330c6aae65f46851b296f6efbd23b20a5d
zoeglbrt <zoegilbert.design@gmail.com>
2025-05-22 16:41:30 +0800

    created colors and tokens files

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const pink40 = Color(0x66FFD1FF);
+  static const pink100 = Color(0xFFFFD1FF);
+
+  static const black5 = Color(0x0D2E2E2E);
+  static const black10 = Color(0x1A2E2E2E);
+  static const black40 = Color(0x662E2E2E);
+  static const black60 = Color(0x992E2E2E);
+  static const black80 = Color(0xCC2E2E2E);
+  static const black100 = Color(0xFF2E2E2E);
+
+  static const white100 = Color(0xFFFFFFFF);
+  static const white200 = Color(0xFFFBFBFB);
+
+  static const pastelYellow = Color(0xFFFFF1AD);
+  static const pastelBlue = Color(0xFFDBEFFF);
+  static const pastelRed = Color(0xFFFFC8C1);
+  static const pastelGreen = Color(0xFFC0FFCE);
+  static const pastelPurple = Color(0xFFF0D5FF);
+  static const pastelOrange = Color(0xFFFFDFC1);
+
+  static const strongRed = Color(0xFFF95F5F);
+  static const strongGreen = Color(0xFF00D4A1);
+  static const strongBlue = Color(0xFF41A7FF);
+}
+
+
+final searchColor = Colors.pink.withAlpha(13);
+
+final bgColor = Colors.pink[300];
---
+++ lib/core/theme/tokens.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTokens {
+  // TEXT
+  static const textPrimary = AppColors.black100;
+  static const textSecondary = AppColors.black40;
+  static const textDisabled = AppColors.black40;
+
+  // BACKGROUND
+  static const bgPrimary = AppColors.white100;
+  static const bgMuted = AppColors.white200;
+  static const bgElevated = AppColors.white100;
+
+  // BORDER
+  static const borderLight = AppColors.black10;
+  static const borderStrong = AppColors.black60;
+
+  // BUTTON PRIMARY
+  static const buttonPrimaryBg = AppColors.pink100;
+  static const buttonPrimaryText = AppColors.black100;
+
+  // BUTTON SECONDARY
+  static const buttonSecondaryBg = AppColors.black5;
+  static const buttonSecondaryText = AppColors.black100;
+
+  // ICONS
+  static const iconPrimary = AppColors.black100;
+  static const iconMuted = AppColors.black40;
+
+  // FEEDBACK STATES
+  static const stateSuccess = AppColors.strongGreen;
+  static const stateError = AppColors.strongRed;
+  static const stateInfo = AppColors.strongBlue;
+}
